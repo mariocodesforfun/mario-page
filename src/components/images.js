bc87a0f03dc18c42d56398539dc502bdf4368baf
mariocodesforfun <gegpriftimario@gmail.com>
2024-12-29 22:49:58 -0500

    Updates

--- src/components/images.js
+++
@@ -1,9 +0,0 @@
-import baruchLogo from './images/Baruch_College_stacked.svg.png';
-import lagccLogo from './images/lagcc_logo_off.jpg';
-import rexeraLogo from './images/inspecthoa_rexera.png';
-const images = {
-    baruch_logo: baruchLogo,
-    lagcc_logo: lagccLogo,
-    rexera_logo: rexeraLogo,
-};
-export default images;
