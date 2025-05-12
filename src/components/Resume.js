5da762c6c944f05ac0327264f6be1b16e93622c7
mariocodesforfun <gegpriftimario@gmail.com>
2025-05-12 11:05:49 -0400

    add new experience

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,13 +6,41 @@ function Resume() {
   return (
     <div className="resume">
       <h1 className="page-title">Resume</h1>
-      <div className="resume-section education-section">
+
+      <div className="resume-section experience-section">
+      <h2 className="section-title">Experience</h2>
+      <div className="resume-entry">
+          <img className="school-logo" src={images.betterment_logo} alt="Betterment Logo" />
+          <div className="school-info">
+            <h3>Betterment</h3>
+            <p>Software Engineer Intern | June 2024 - August 2024</p>
+          </div>
+        </div>
+
+        <div className="resume-entry">
+          <img className="school-logo" src={images.style_ai_logo} alt="Style AI Logo" />
+          <div className="school-info">
+            <h3>Style AI</h3>
+            <p>Founding Engineer | May 2024 - Present</p>
+          </div>
+        </div>
+      <div className="resume-entry">
+            <img className="school-logo" src={images.rexera_logo} alt="Rexera Logo" />
+            <div className="school-info">
+            <h3>Rexera</h3>
+            <p>Software Engineer II | January 2023 - February 2025</p>
+          </div>
+        </div>
+
+        <div className="resume-section education-section">
         <h2 className="section-title">Education</h2>
         <div className="resume-entry">
         <img className="school-logo" src={images.baruch_logo} alt="Baruch College Logo" />
         <div className="school-info">
             <h3>Baruch College</h3>
             <p>Computer Science | Expected Graduation: Summer 2026</p>
+            <p>Dean's List | 2024-2025</p>
+            <p>GPA: 3.8</p>
           </div>
         </div>
 
@@ -22,21 +50,11 @@ function Resume() {
             <h3>LaGuardia Community College</h3>
             <p>Computer Science | March 2023 - June 2024</p>
             <p>Dean's List | 2023-2024</p>
-            <p>GPA: 3.83</p>
+            <p>GPA: 3.8</p>
           </div>
         </div>
         {/* Add more education entries as needed */}
       </div>
-
-      <div className="resume-section experience-section">
-      <h2 className="section-title">Experience</h2>
-      <div className="resume-entry">
-            <img className="school-logo" src={images.rexera_logo} alt="Rexera Logo" />
-            <div className="school-info">
-            <h3>Rexera</h3>
-            <p>Software Engineer II | January 2023 - Present</p>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/components/images.js
+++ src/components/images.js
@@ -1,9 +1,13 @@
 import baruchLogo from './images/Baruch_College_stacked.svg.png';
 import lagccLogo from './images/lagcc_logo_off.jpg';
 import rexeraLogo from './images/inspecthoa_rexera.png';
+import bettermentLogo from './images/betterment_logo.png';
+import styleAiLogo from './images/style_ai_logo.png';
 const images = {
     baruch_logo: baruchLogo,
     lagcc_logo: lagccLogo,
     rexera_logo: rexeraLogo,
+    betterment_logo: bettermentLogo,
+    style_ai_logo: styleAiLogo,
 };
 export default images;
